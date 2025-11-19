4bfd349128b17af6c40f6ad220aaa77a12141421
alaninnovates <alanlichen1@gmail.com>
2025-11-18 23:07:09 -0800

    Migrate to tailwind

--- Gemfile
+++ Gemfile
@@ -41,7 +41,7 @@ gem "thruster", require: false
 gem "image_processing", "~> 1.2"
 
 gem "view_component"
-gem "dartsass-rails", "~> 0.5.1"
+gem "tailwindcss-rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -2,10 +2,10 @@
 
 class ButtonComponent < ViewComponent::Base
   COLORS = %i[red green blue yellow brown].freeze
-  VARIANTS = %i[default borderless].freeze
+  VARIANTS = %i[default].freeze
   SIZES = %i[sm md lg].freeze
 
-  attr_reader :text, :color, :variant, :size, :href, :method, :html_options
+  attr_reader :text, :color, :variant, :size, :href, :type, :method, :html_options
 
   def initialize(
     text: nil,
@@ -13,6 +13,7 @@ class ButtonComponent < ViewComponent::Base
     variant: :default,
     size: :md,
     href: nil,
+    type: nil,
     method: nil,
     **html_options
   )
@@ -21,17 +22,36 @@ class ButtonComponent < ViewComponent::Base
     @variant = normalize(:variant, variant, VARIANTS)
     @size = normalize(:size, size, SIZES)
     @href = href
+    @type = type
     @method = method
     @html_options = html_options
   end
 
   def button_classes
-    classes = {
-      "btn" => true,
-      "btn--#{color}" => color.present?,
-      "btn--#{variant}" => variant != :default,
-      "btn--#{size}" => size != :md
+    classes = []
+    classes << "rounded-md"
+
+    color_map = {
+      red: "bg-red-500 hover:bg-red-600 text-white focus:ring-red-500",
+      green: "bg-green-500 hover:bg-green-600 text-white focus:ring-green-500",
+      blue: "bg-blue-500 hover:bg-blue-600 text-white focus:ring-blue-500",
+      yellow: "bg-yellow-400 hover:bg-yellow-500 text-black focus:ring-yellow-400",
+      brown: "bg-amber-700 hover:bg-amber-800 text-white focus:ring-amber-700"
     }
+    classes << color_map[color] if color.present?
+
+    variant_map = {
+      default: "",
+    }
+    classes << variant_map[variant] if variant != :default
+
+    size_map = {
+      sm: "px-4 py-3 text-sm",
+      md: "px-4 py-3 text-base",
+      lg: "px-4 py-3 text-lg"
+    }
+    classes << size_map[size]
+
     class_names(classes, html_options[:class])
   end
 
