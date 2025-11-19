38a98c89f1774ab9e802cb5d2580fb910bf9d8da
alaninnovates <alanlichen1@gmail.com>
2025-11-18 20:46:00 -0800

    Fully implement auth flow

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "rails_db"
 end
 
 group :test do
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: %i[ new create ]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_registration_path, alert: "Try again later." }
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      start_new_session_for @user
+      redirect_to after_authentication_url, notice: "Account created successfully."
+    else
+      redirect_to new_registration_path, alert: @user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+    def user_params
+      params.expect(user: [:email, :password])
+    end
+end
