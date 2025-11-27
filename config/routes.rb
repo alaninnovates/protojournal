3ef171da86325c4d6a15d7f672675265421fa3b1
alaninnovates <alanlichen1@gmail.com>
2025-11-27 11:48:12 -0800

    Add photos views/controller and add icons

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
 
   resources :projects
+  resources :photos
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
